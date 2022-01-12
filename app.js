78f9cb21e184810298facd424463de838ae07644
Md. Razu Haolader <razu@Tipus-Air.localdomain>
2022-01-12 08:38:00 +0600

    add dotenv

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const  logger = require('./logger');
 const loggerObj = new logger();
+require('dotenv').config();
 
 loggerObj.on('saveUserData',(data) => {
-    console.log('User Data saved successfully');
+    console.log(`User Data saved successfully by ${process.env.APP_USER_NAME}`);
     console.log(data);
 });
 
